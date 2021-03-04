6b945413a2b597180244caf5998761fc4fb3b0fc
Joseph Wojtowicz <josephwojtowicz@Josephs-MacBook-Pro.local>
2021-03-04 11:52:37 -0500

    idk man

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT  = process.env.PORT || 3001;
 //middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //connect mongo
 const uri = process.env.MONGODB_URI;

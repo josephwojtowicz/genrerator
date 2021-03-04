3ff0fcd0be29ea152e693f7f46e214099321af9e
Joseph Wojtowicz <josephwojtowicz@Josephs-MacBook-Pro.local>
2021-03-04 12:06:16 -0500

    please

--- server.js
+++ server.js
@@ -29,6 +29,12 @@ const genresRouter = require('./routes/genres');
 
 app.use('/genres', genresRouter);
 
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 app.listen(PORT, function() {
     console.log(`now listening on PORT ${PORT}`)
 });
